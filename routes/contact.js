3ac9b5639dc64d0225f74deba0844520f981b963
vgnsh1999 <vigneshvarathan@gmail.com>
2023-07-04 12:09:08 +0530

    Added controller in /contactus

--- routes/contact.js
+++ routes/contact.js
@@ -1,22 +1,15 @@
 //creating path
-const path = require('path');
 
 const express = require('express');
 
-//adding rootDir
-const rootDir = require('../util/path');
+//removing rootDir and adding controller
+//const rootDir = require('../util/path');
+const contactController = require('../controllers/contactcontrol');
 
 const router = express.Router();
 
-router.get('/',(req,res,next)=> {
-    //res.sendFile(path.join(__dirname,'../','views','contact.html')); //with /
-    //res.sendFile(path.join(__dirname,'..','views','contact.html')); //without /
-    res.sendFile(path.join(rootDir,'views','contact.html')); //adding rootDir
-});
+router.get('/',contactController.getContact);
 
-router.post('/',(req,res,next)=>{
-    console.log(req.body); // we have to parse else we get undefined
-    res.redirect('/success');
-});
+router.post('/',contactController.postContact);
 
 module.exports = router;
